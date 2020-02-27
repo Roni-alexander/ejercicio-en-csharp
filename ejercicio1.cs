25c8a5ead505b648ababfa5088c081ab607e3104
Roni-alexander <61532316+Roni-alexander@users.noreply.github.com>
2020-02-27 18:38:22 -0400

    Add files via upload

---
+++ ejercicio1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int variable1 = 4;
+            int variable2 = 20;
+            int variable3 = 6;
+            
+            Console.WriteLine("el resultado de la suma de las tres variables es:");
+            
+            Console.WriteLine(variable1+variable2+variable3);
+
+            Console.ReadKey();
+        }
+    }
+}
