00482c08e58312b7141d8192efdd3b8c78f34708
Roni-alexander <61532316+Roni-alexander@users.noreply.github.com>
2020-02-27 11:49:30 -0400

    Add files via upload

---
+++ ejercicio 2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tarea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre;
+            int edad;
+
+            Console.WriteLine("ingrese su nombre:");
+            nombre = Console.ReadLine();
+            Console.WriteLine("ingrese su edad:");
+            edad = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("su nombre y su edad es:"+nombre);
+            Console.WriteLine("la edad es: "+edad);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ejercicio 4.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int f;
+            for (f = 1; f <= 50; f++)
+            {
+                Console.Write(f);
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+        
+    }
+}
